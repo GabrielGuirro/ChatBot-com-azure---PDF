c413b7e5ebe3ce02913c0ab6ac0990bc2f8d5b5a
GabrielGuirro <76258211+GabrielGuirro@users.noreply.github.com>
2025-03-11 11:27:27 -0300

    Create azure_config.py

---
+++ azure_config.py
@@ -0,0 +1,7 @@
+# Configurações do Azure
+AZURE_ENDPOINT = "https://<seu-serviço-cognitive-search>.search.windows.net"
+AZURE_API_KEY = "<sua-chave-api-azure>"
+INDEX_NAME = "pdf-index"
+
+# Configurações do Azure OpenAI
+OPENAI_API_KEY = "<sua-chave-api-openai>"
